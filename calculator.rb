5bebdc393a3117e87d1795ca7fb0c5f4e39b6d80
Ian McKenzie <imckenzie777@gmail.com>
2020-04-26 23:58:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,5 @@ first_number = 25
 second_number = 14.5
 sum = first_number + second_number
 difference = first_number - second_number
-#product = first_number *= second_number
+product = first_number * second_number
 #quotient = first_number /= secon_number
